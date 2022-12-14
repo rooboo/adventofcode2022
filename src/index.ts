99d8db8c87d00b3df94f1f33256e0c8a2511808a
rooboo <nig_niggin@gmx.net>
2022-12-14 18:27:51 +0100

    async getInput; more strict tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import solveDay1 from './1';
 import solveDay2 from './2';
 
-console.log('Solutions to day 1:');
 solveDay1();
-console.log('\n');
-console.log('Solutions to day 2:');
 solveDay2();
